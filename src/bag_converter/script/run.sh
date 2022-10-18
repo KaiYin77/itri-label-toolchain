fe3c08fac27ed9d1dee94b1b4203b56aa3ebadf2
Kevin Hong <kevinhong@inquartik.com>
2022-10-19 00:03:07 +0800

    move to raw/data

--- src/bag_converter/script/run.sh
+++ src/bag_converter/script/run.sh
@@ -1,4 +1,4 @@
-roslaunch perception-offline-test-label-data.launch bags:=/itri-label-toolchain/src/data/2022-08-17-16-10-32_13.bag
-python ros_image_saver.py --topic /lucid_cameras_x00/gige_30_f_hdr/h265/rgb8 --save_dir /itri-label-toolchain/src/data
-python ros_image_saver.py --topic /lucid_cameras_x00/gige_60_f_hdr/h265/rgb8 --save_dir /itri-label-toolchain/src/data
-rosrun pcl_ros bag_to_pcd /itri-label-toolchain/src/data/2022-08-17-16-10-32_13.bag /velodyne/top_vls128 /itri-label-toolchain/src/data/lidar
+roslaunch perception-offline-test-label-data.launch bags:=/itri-label-toolchain/src/data/raw/2022-08-17-16-10-32_13.bag
+python ros_image_saver.py --topic /lucid_cameras_x00/gige_30_f_hdr/h265/rgb8 --save_dir /itri-label-toolchain/src/data/raw
+python ros_image_saver.py --topic /lucid_cameras_x00/gige_60_f_hdr/h265/rgb8 --save_dir /itri-label-toolchain/src/data/raw
+rosrun pcl_ros bag_to_pcd /itri-label-toolchain/src/data/2022-08-17-16-10-32_13.bag /velodyne/top_vls128 /itri-label-toolchain/src/data/raw/lidar
